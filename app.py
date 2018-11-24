9bb9e79c065dd67fd8c4db9ade16f27a4503e3bc
Michael Silver <michaelssilver@me.com>
2018-11-24 20:27:16 +0200

    Delete commented code

--- app.py
+++ app.py
@@ -22,33 +22,6 @@ colorscale = [ [ float(i)/float(len(scl)-1), scl[i] ] for i in range(len(scl)) ]
 app.layout = html.Div(children=[
     html.H1(children='Shoppy Insights',style={'textAlign': 'center'}),
 
-    # dcc.Graph(
-    #     id='location-graoh',
-    #     figure={
-    #         'data': [
-    #             go.Scatter(
-    #                 x=df['x'],
-    #                 y=df['y'],
-    #                 # text='User: {}'.format(df['userid']),
-    #                 text=df['userid'],
-    #                 mode='markers',
-    #                 opacity=0.7,
-    #                 # name=df['userid'],
-    #                 # marker={
-    #                 #     'size': 15,
-    #                 #     'line': {'width': 0.5, 'color': 'white'}
-    #                 # },
-    #             )
-    #         ],
-    #         'layout': go.Layout(
-    #             # xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-    #             # yaxis={'title': 'Life Expectancy'},
-    #             # margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-    #             # legend={'x': 0, 'y': 1},
-    #             hovermode='closest'
-    #         )
-    #     }
-    # ),
     dcc.Graph(
         id='density-graph',
         figure={
@@ -105,53 +78,6 @@ app.layout = html.Div(children=[
             )
         }
     ),
-    # dcc.Graph(
-    #     id='scatter-graph',
-    #     figure={
-    #         'data': [
-    #             go.Scatter(
-    #                 x=df['x'],
-    #                 y=df['y'],
-    #                 name='density',
-    #                 mode='markers',
-    #                 # ncontours=20,
-    #                 # colorscale=colorscale,
-    #                 # showscale=False
-    #             ),
-    #         ],
-    #         'layout': go.Layout(
-    #             images= [dict(
-    #                 source= 'assets/store_with_grid.jpg',
-    #                 xref= "paper",
-    #                 yref= "paper",
-    #                 x= 0,
-    #                 y= 1,
-    #                 sizex= 1,
-    #                 sizey= 1,
-    #                 sizing= "stretch",
-    #                 opacity= 0.5,
-    #                 layer= "above")],
-    #             showlegend=False,
-    #             autosize=False,
-    #             width=700,
-    #             height=700,
-    #             hovermode='closest',
-    #             bargap=0,
-
-    #             # showticklabels
-    #             xaxis=dict(range=[0,20],domain=[0, 1], linewidth=2, linecolor='#444',
-    #                        showgrid=False, zeroline=False, ticks='', showticklabels=True, showline=True, mirror=True),
-
-    # yaxis=dict(range=[0,23],domain=[0, 1],linewidth=2,linecolor='#444',
-    #                        showgrid=False, zeroline=False, ticks='', showticklabels=True, showline=True, mirror=True),
-    # xaxis2=dict(range=[0,20], domain=[0, 1], showgrid=False, zeroline=False, ticks='',
-    #             showticklabels=False ),
-
-    # yaxis2=dict(range=[0,23], domain=[0, 1], showgrid=False, zeroline=False, ticks='',
-    #             showticklabels=False ),
-    #         )
-    #     }
-    # )
 ])
 
 if __name__ == '__main__':
