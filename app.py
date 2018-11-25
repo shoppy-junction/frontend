7ee9968513e943eb2e5ba87522340719d2ec6dc5
Michael Silver <michaelssilver@me.com>
2018-11-25 05:17:06 +0200

    Add slider

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+from datetime import datetime
 # import colorlover as cl
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -13,6 +15,10 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # df = pd.read_csv('processed_output.csv')
 df = pd.read_csv('../backend/data/location_data_processed.csv')
 df2 = pd.read_csv('../backend/data/purchase_data_processed.csv')
+
+min_time = min(df["timestamp"].min(), df2["timestamp"].min())
+max_time = max(df["timestamp"].max(), df2["timestamp"].max())
+
 moneys = df2.groupby(['x','y'])
 moneys_points = list(moneys.groups.keys())
 df3 = pd.read_csv('../backend/data/noisy_location_processed.csv')
@@ -27,6 +33,21 @@ df3 = pd.read_csv('../backend/data/noisy_location_processed.csv')
 
 app.layout = html.Div(children=[
     html.H1(children='Shoppy Insights',style={'textAlign': 'center'}),
+
+    html.H5(
+        '',
+        id='time_range_text',
+        style={'text-align': 'right'}
+    ),
+
+    dcc.RangeSlider(
+        id='time_slider',
+        # count=1,
+        # step=1,
+        min=min_time,
+        max=max_time,
+        value=[min_time, max_time]
+    ),
 
     # html.Div(
     # [
@@ -158,6 +179,13 @@ app.layout = html.Div(children=[
         }
     )
 ])
+
+# Slider -> time text
+@app.callback(Output('time_range_text', 'children'),
+              [Input('time_slider', 'value')])
+def update_time_text(time_slider):
+    return "{} - {}".format(datetime.fromtimestamp(time_slider[0]).strftime('%H:%M:%S'), datetime.fromtimestamp(time_slider[1]).strftime('%H:%M:%S on %a, %b %d'))
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
